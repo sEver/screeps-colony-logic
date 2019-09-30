622b392b75ad3c8fc95ce8290b7d27b601f4631a
sEver <sEver@users.noreply.github.com>
2019-09-30 12:38:04 +0200

    Define roles within config

--- screeps.com/AI-SHA/aisha.config.js
+++ screeps.com/AI-SHA/aisha.config.js
@@ -5,9 +5,21 @@ module.exports = {
       body: [WORK,CARRY,MOVE],
     },
   },
+  creepRoles: {
+    harvester: {
+      actions: ["harvest", "store", "upgrade"],
+    },
+    upgrader: {
+      actions: ["harvest", "upgrade"],
+    },
+    builder: {
+      actions: ["retrieve", "build"],
+    }
+  },
   targetNumberOfRoles: {
     drone: 2,
     upgrader: 1,
+    builder: 0,
   },
   minimumStructureHp: 2000,
 }
